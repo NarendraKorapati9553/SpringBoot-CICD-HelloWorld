4e0c63881ae9b2ee2b46f1e0f0248097715d3715
Narendra Korapati <nkp9553@gmail.com>
2020-05-19 13:31:04 +0530

    adding all the files for the first timeand all the configurations also getting included

---
+++ spring-CICD-HelloWorld/src/main/java/com/example/demo/SpringCicdHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCicdHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCicdHelloWorldApplication.class, args);
+	}
+
+}
